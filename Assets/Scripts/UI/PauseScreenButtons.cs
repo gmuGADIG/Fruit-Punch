83300fc8c23fb5b52fac7c6e5ace8bf29fd30e6a
CarJasper <cjasper@gmu.edu>
2024-01-31 20:45:40 -0500

    PuaseScreenCodeOutline
    A basic code outline for the puase screens button functions

--- Assets/Scripts/UI/PauseScreenButtons.cs
+++ Assets/Scripts/UI/PauseScreenButtons.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseScreenButtons : MonoBehaviour
 {
+    public string mainMenuScene;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 0.0f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyUp(KeyCode.Escape))
+        { Resume(); }
+    }
+    public void Resume() //closes pause screen resumes game
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.UnloadSceneAsync("PauseScreen");
+        Debug.Log("PauseScreen unloaded");
+    }
+
+    public void RestartLevel() //restarts level
+    {
+        for(int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            if(SceneManager.GetSceneAt(i).name != "PauseScreen")
+            {
+                SceneManager.LoadSceneAsync(SceneManager.GetSceneAt(i).name);
+            }
+            Time.timeScale = 1.0f;
+            SceneManager.UnloadSceneAsync("PauseScreen");
+            Debug.Log("RestartedLevel");
+        }
+    }
+
+    public void MainMenu() //returns to mainMenu
+    {
+        SceneManager.LoadScene(mainMenuScene);
+        Debug.Log("Return to Main");
+    }
+    public void QuitGame() //Quits the game
+    {
+        //add script to save game
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+
+    public void OptionsMenu() //Opens options menu
+    {
+        Debug.Log("Open Options Menu");
     }
 }
