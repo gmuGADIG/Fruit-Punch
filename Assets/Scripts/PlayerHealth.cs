ec327f980b7ac0b4a12b2ac332f3b7c2d1f853b4
OracleBlue5 <christiantbo578@gmail.com>
2024-01-31 21:14:48 -0500

    Basic HP Bar
    Made basic hp bar that updates every frame. Can improve by invoking event listeners whenever you take damage

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,30 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    private int health;
-    private int maxHealth;
+    [SerializeField] private int health = 100;
+    [SerializeField] private int maxHealth = 100;
 
-    public void removeHealth(int damage){
-        health -= damage;
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private TMP_Text healthBarText;
+
+    void Start()
+    {
+        healthSlider.GetComponent<Slider>().value = CalculateSliderPercentage(health, maxHealth);
+        healthBarText.text = "HP " + health + " / " + maxHealth;
     }
 
-      public void addHealth(int heal){
-        health += heal;
-    }
-
-    
-    // Update is called once per frame
     void Update()
     {
-        //when a player's health drops below 0 they die
-        if(health <= 0){
+        if (health > 0)
+        {
+            healthSlider.GetComponent<Slider>().value = CalculateSliderPercentage(health, maxHealth);
+            healthBarText.text = "HP " + health + " / " + maxHealth;
+        } else
+        {
             health = 0;
             print("you died :3");
         }
+    }
 
+    private float CalculateSliderPercentage(int playerHealth, int maxPlayerHealth)
+    {
+        return (float)playerHealth / maxPlayerHealth;
+    }
 
+    public void removeHealth(int damage)
+    {
+        health -= damage;
+    }
+
+    public void addHealth(int heal)
+    {
+        health += heal;
     }
 }
