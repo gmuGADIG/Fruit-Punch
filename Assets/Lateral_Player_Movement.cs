be80bd5f355689ea0d01f7bbf6fd9a2ba3ebd15c
MMillion-vmat-c <max.fowlkes@gmail.com>
2024-01-31 21:14:11 -0500

    added basic left/right movement
    Needs to eventually be updated with the PlayerInput system but we aren't at that stage yet.

--- Assets/Lateral_Player_Movement.cs
+++ Assets/Lateral_Player_Movement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Lateral_Player_Movement : MonoBehaviour
 {
+
+    public float speed = 10f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Input.GetAxisRaw("Horizontal");
+        // Get only horizontal input, add it onto the existing X position without touching any other axis.
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.position+= new Vector3(horizontalInput*speed*Time.deltaTime,0,0);
+
     }
 }
