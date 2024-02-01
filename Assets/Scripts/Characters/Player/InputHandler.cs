3b8e674a4df6366f4c05655005bbc553787295c2
Michael Campbell <rainbow@portal2d.xyz>
2024-01-31 19:50:38 -0500

    add input plugin

---
+++ Assets/Scripts/Characters/Player/InputHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InputType {
+    LeftKeyboard,
+    RightKeyboard
+}
+
+public class InputHandler : MonoBehaviour
+{
+
+}
