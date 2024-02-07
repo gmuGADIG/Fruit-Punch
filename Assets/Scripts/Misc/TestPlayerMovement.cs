f480c25027e9a793e4f6e34a28e30b7060e6cec3
Michael Campbell <rainbow@portal2d.xyz>
2024-02-07 18:40:00 -0500

    input rebinding example

--- Assets/Scripts/Misc/TestPlayerMovement.cs
+++ Assets/Scripts/Misc/TestPlayerMovement.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,8 +11,7 @@ public class TestPlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Vector2 moveDir = Vector2.zero;
-    private InputAction move;
-    private InputAction fire;
+    public InputActionReference strike;
 
     PlayerInput input;
 
@@ -30,5 +31,42 @@ public class TestPlayerMovement : MonoBehaviour
         ), 1);
 
         transform.position += moveDir * Time.fixedDeltaTime * 2f * moveSpeed;
+    }
+
+    public void OnStrike() {
+        Debug.Log("Strike Pressed!");
+    }
+
+    public void OnDebugRebindControl() {
+        Debug.Log("DebugRebindControl Pressed!");
+
+        var index = strike.action.bindings
+            .Select((b, index) => (Binding: b, Index: index))
+            .Where(t => t.Binding.groups == input.currentControlScheme)
+            .Select(t => t.Index)
+            .First();
+        
+        var bindings = strike.action.bindings;
+        
+        Debug.Log($"index: {index}");
+
+        var actionIter = input.actions.actionMaps.Aggregate(
+            new List<InputAction>().AsEnumerable(),
+            (aux, next) => aux.Concat(next).Concat(next)
+        );
+        var action = actionIter
+            .Where(action => action.id == strike.action.id)
+            .First();
+
+        action.Disable();
+        action.PerformInteractiveRebinding()
+            .OnComplete(operation => {
+                Debug.Log("Rebinding complete!");
+
+                action.Enable();
+
+                operation.Dispose(); // manual memory management: c# is not a real language
+            })
+            .Start();
     }
 }
