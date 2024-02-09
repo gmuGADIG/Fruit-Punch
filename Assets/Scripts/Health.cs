9269055bd60326aa68a2cd9c5f5332311ada26c9
Justin1L8 <justin.w.langdon@gmail.com>
2024-02-08 21:15:12 -0500

    Added aura support, made events public, removed player-specific code
    Damage function now uses the new DamageInfo class, making it more flexible if more info is needed in the future.
    Health class now supports auras. Attacks are ignored if the character isn't vulnerable to that aura. Players should be vulnerable to all, and enemy attacks should use AuraType.EnemyAtk.
    The Health script no longer cares if it's attached the player. That's for other scripts to worry about.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -19,70 +19,83 @@ Magic of Events Implemented by Justin from designed code.
 
 public class Health : MonoBehaviour
 {
-
+    public AuraType vulnerableTypes;
+    
     [SerializeField]
     public float maxHealth = 100;
-    [ReadOnlyInInspector]
+    
+    [ReadOnlyInInspector, SerializeField]
     private float currentHealth;
-
-    private bool isPlayer;
+    
+    /// <summary>
+    /// Invoked when this character's health reaches zero. <br/>
+    /// (run after onHealthChange and onHurt)
+    /// </summary>
+    public event Action onDeath;
 
     /// <summary>
-    /// Invoked when this character's health reaches zero.
+    /// Invoked with the amount of damage and knockback whenever hurt. <br/>
+    /// (run after onHealthChange and before onDeath)
     /// </summary>
-    private event Action onDeath;
+    public event Action<DamageInfo> onHurt;
 
     /// <summary>
-    /// Invoked with the amount of damage and knockback whenever hurt.
+    /// Invoked with the new health whenever it's changed, positively or negatively. <br/>
+    /// (run before onHurt and onDeath)
     /// </summary>
-    private event Action<float, Vector2> onHurt;
+    public event Action<float> onHealthChange;
 
-    /// <summary>
-    /// Invoked when the player health is changed to update the health bar.
-    /// </summary>
-    private event Action onPlayerHealth;
+    public event Action<DamageInfo> onDamageImmune;
     
     void Start()
     {
         currentHealth = maxHealth;
-        if(this.tag == "Player"){
-            isPlayer = true;
-        } else
-        {
-            isPlayer = false;
-        }
     }
     
     /// <summary>
-    /// Damages the character, removing it's health. Knockback should be handled in a separate script.
+    /// Attempts to damage the character, decreasing its health towards zero (no lower). <br/>
+    /// Factors in the aura of the attack, and ignoring it's damage if this character isn't vulnerable. <br/>
+    /// Doesn't do anything with knockback; that's up the player or enemy script. <br/>
+    /// (onHealthChange, onHurt, and onDeath are invoked if applicable)
     /// </summary>
-    public void Damage(float amount, Vector2 knockback)
+    public void Damage(DamageInfo info)
     {
-        if(currentHealth - amount <= 0)
+        if (this.currentHealth <= 0) return; // don't die twice. probably gonna be convenient later.
+        if (!IsVulnerableTo(info.aura))
         {
-            currentHealth = 0;
-        }else
-        {
-            currentHealth -= amount;
+            onDamageImmune?.Invoke(info);
+            return;
         }
-        if (isPlayer)
-        {
-            onPlayerHealth?.Invoke();
-        }
-        onHurt.Invoke(amount, knockback);
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
+        
+        currentHealth = Mathf.MoveTowards(currentHealth, 0, info.damage);
+        
+        onHealthChange?.Invoke(currentHealth);
+        onHurt?.Invoke(info);
+        
+        if (currentHealth <= 0) Die();
     }
 
+    /// <summary>
+    /// Returns true if this character is vulnerable to an attack's aura.
+    /// </summary>
+    bool IsVulnerableTo(AuraType attackingAura)
+    {
+        // vuln = 0010, atk = 1111 --> effective = 0010; return true
+        // vuln = 1111, atk = 0001 --> effective = 0001; return true
+        // vuln = 0000, atk = 1111 --> effective = 0000; return false
+        var effectiveAuras = this.vulnerableTypes & attackingAura;
+        return effectiveAuras != 0;
+    }
+
+    /// <summary>
+    /// Heals the character, increasing its health towards the maximum (no higher). <br/>
+    /// (onHealthChange is invoked with the new health value)
+    /// </summary>
+    /// <param name="amount"></param>
     public void Heal(float amount)
     {
-        if (isPlayer)
-        {
-            onPlayerHealth?.Invoke();
-        }
         currentHealth = Mathf.MoveTowards(currentHealth, maxHealth, amount);
+        onHealthChange?.Invoke(currentHealth);
     }
 
     private void Die()
