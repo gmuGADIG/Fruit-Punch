4562aa1e7acf7463ae6653edb26fbb7fae0ee317
KaratePikachu <KaidenKeyZ@gmail.com>
2024-02-14 19:57:29 -0500

    Player Image Cycle Tweaks
    Added error checking for the array and enum sizes matching

--- Assets/Scripts/Characters/Player/Select/TestPlayerSelector.cs
+++ Assets/Scripts/Characters/Player/Select/TestPlayerSelector.cs
@@ -6,24 +6,54 @@ using UnityEngine.UI;
 
 public class TestPlayerSelector : MonoBehaviour
 {
+    enum Character
+    {
+        Apple,
+        Banana,
+        Orange
+    }
+    
     public GameObject playerOneSelector;
     public GameObject playerTwoSelector;
     public bool playerOneJoined = false;
     public bool playerTwoJoined = false;
     [SerializeField]
     public Sprite[] images;
-    private Image img;
-    int imgIndex = 0;
+    private Image characterImage;
+    int numCharacters;
+    Character character;
 
     private void Start()
     {
-        img = transform.Find("Character Image").GetComponent<Image>();
+        characterImage = transform.Find("Character Image").gameObject.GetComponent<Image>();
+        numCharacters = Character.GetNames(typeof(Character)).Length;
+        if (numCharacters != images.Length)
+            Debug.LogWarning("TestPlayerSelector.cs: The player enum and images provided do not have equivilent length.");
+        
+        character = Character.Apple;
+        characterImage.sprite = images[0];
     }
 
     void OnUp()
     {
-        img.sprite = images[++imgIndex];
+        character++;
+        if ((int)character >= numCharacters)
+            character = (Character)0;
+        characterImage.sprite = images[(int)character];
     }
 
-    
+    void OnDown()
+    {
+        character--;
+        if (character < 0)
+            character = (Character)(numCharacters - 1);
+        characterImage.sprite = images[(int)character];
+    }
+
+    void OnConfirm()
+    {
+        Debug.Log("Confirm");
+    }
+
+
 }
