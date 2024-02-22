7fbdac34b9098f380be8ad3374529224719ffaa5
Brendan Halstead <Brendan_Halstead@live.com>
2024-02-21 20:25:20 -0500

    manager stores stuff and fixed return error

--- Assets/Scripts/Characters/Player/Select/TestPlayerSelectorSpawner.cs
+++ Assets/Scripts/Characters/Player/Select/TestPlayerSelectorSpawner.cs
@@ -8,6 +8,10 @@ public class TestPlayerSelectorSpawner : MonoBehaviour
     public GameObject prefab;
     public GameObject blankPOneSelector;
     public GameObject blankPTwoSelector;
+    /// <summary>
+    /// game obejct in scene that has <see cref="CharacterSelectorManager"/>.
+    /// </summary>
+    public GameObject manager;
     public void ConnectPlayer(JoinContext context)
     {
         PlayerInput p = PlayerInput.Instantiate(
@@ -23,7 +27,7 @@ public class TestPlayerSelectorSpawner : MonoBehaviour
             p.gameObject.transform.parent = GameObject.Find("Canvas").transform;
             p.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-250, -100, 0);
             Destroy(blankPOneSelector);
-            GameObject.Find("Manager").GetComponent<CharacterSelectorManager>().playerOneSelector = p.gameObject;
+            manager.GetComponent<CharacterSelectorManager>().playerOneSelector = p.gameObject;
             GameManager.gameManager.playerOneInputDevice  = context.InputDevice;
             GameManager.gameManager.playerTwoControlScheme = context.ControlScheme;
         }
@@ -32,10 +36,10 @@ public class TestPlayerSelectorSpawner : MonoBehaviour
             p.gameObject.transform.parent = GameObject.Find("Canvas").transform;
             p.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(250, -100, 0);
             Destroy(blankPTwoSelector);
-            GameObject.Find("Manager").GetComponent<CharacterSelectorManager>().playerTwoSelector = p.gameObject;
+            manager.GetComponent<CharacterSelectorManager>().playerTwoSelector = p.gameObject;
             GameManager.gameManager.playerOneInputDevice = context.InputDevice;
             GameManager.gameManager.playerOneControlScheme = context.ControlScheme;
         }
-        return null;
+        
     }
 }
