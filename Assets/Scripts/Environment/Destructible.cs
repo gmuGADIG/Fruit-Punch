840a9ba3f98a696d30fae9081994ea4f47c52656
JoshuaLSime <jguysime@gmail.com>
2024-02-21 20:01:09 -0500

    Created Destructible Script
    Created a Destructible Script to be applied to objects we want the player to destroy. Since attack functionality has not been implemented yet objects are destroyed when the player collides with them at all.
    
    Required actions for functionality:
    Attach 2D collider component. Assign player and damgePerHit with values in inspector. Attach Health and BeltCharacter scripts to the object to be destroyed. Must set a Max Health value in health script and Vulnerable Types to Strike

---
+++ Assets/Scripts/Environment/Destructible.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    // Required actions:
+    // Attach 2D collider component
+    // Assign player and damgePerHit with values in inspector
+    // Attach Health and BeltCharacter scripts to the object to be destroyed
+    // Must set a Max Health value in health script and Vulnerable Types to Strike
+
+    public GameObject player;
+    public float damagePerHit;
+
+    private BeltCharacter beltCharacter;
+    private Health health;
+    private List<BeltCharacter> collisions;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Getting BeltCharacter script from this game object in order to use GetOverlappingBeltCharacters function for collision
+        beltCharacter = gameObject.GetComponent<BeltCharacter>();
+
+        // Getting this objects health
+        health = gameObject.GetComponent<Health>();
+
+        // subscribing obstacleDestroyed() to health scripts onDeath Action
+        health.onDeath += obstacleDestroyed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if obstacle is colliding with anything
+        collisions = beltCharacter.GetOverlappingBeltCharacters(gameObject.GetComponent<Collider2D>(), Physics2D.AllLayers);
+        foreach (BeltCharacter i in collisions)
+        {
+            // If the collision is with the player then destroy this gameobject
+            // If we want the obstacle to be destroyed only when player attackes it edit the If statement below
+            // ie: adding "&& player.attackScriptName.isAttacking"
+            if ( i.tag == "Player")
+            {
+                // Instantiate obstacle being destroyed animations here
+
+                // Does damage to the object this script is attached to with zero knockback and a strike type
+                health.Damage(new DamageInfo(damagePerHit, Vector2.zero, AuraType.Strike));
+            }
+        }
+    }
+
+    public void obstacleDestroyed()
+    {
+        Destroy(gameObject);
+    }
+    
+}
