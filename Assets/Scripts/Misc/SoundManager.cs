3d49f1c00575a93aed15744a288090a0649bfb45
Patrick <logicalrocketship@gmail.com>
2024-03-13 21:23:24 -0400

    Start Over
    All code has been deleted to redo the code.
    A more efficient method for implementing the sound manager will be developed.

--- Assets/Scripts/Misc/SoundManager.cs
+++ Assets/Scripts/Misc/SoundManager.cs
@@ -3,40 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(PlayerInput))]
 public class SoundManager : MonoBehaviour
 {
-    private PlayerInput playerInput;
-    private Grabber grabber;
+    /** START OVER 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        NormalUpdate();
-    }
-
-    // Update is called once per frame
-    void NormalUpdate()
-    {
-        // Print statements to be used as placeholders for sounds
-        if (playerInput.actions["gameplay/Jump"].triggered)
-        {
-            print("Jump sound");
-        }
-        
-        if (playerInput.actions["gameplay/Strike"].triggered)
-        {
-            print("Strike sound");
-        }
-
-        if (playerInput.actions["gameplay/Pearry"].triggered)
-        {
-            print("Pearry sound");
-        }
-        
-        if (playerInput.actions["gameplay/Interact"].triggered)
-        {
-            if (grabber.GrabItem()) print("Grabbing sound");
-        } 
-    }
+    Create a series of static mehtods to play a certain sound. 
+    Implement the sound methods in the method using the sound.
+    
+    */
 }
