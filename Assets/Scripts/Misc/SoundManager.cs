37c3d9afccfeb074ff60f43b3cb8d9c3e593eb8e
Patrick <logicalrocketship@gmail.com>
2024-03-13 20:42:39 -0400

    Sound Manager Implemented
    The script for the Sound Manager has been
    implemented based on the current player state.
    Sound assets have not been developed, so print
    statements are being used as a placeholder
    
    Also moved the SoundManager script to
    Assets/Scripts/Misc.
    It was in the wrong place...

--- Assets/Scripts/Misc/SoundManager.cs
+++ Assets/Scripts/Misc/SoundManager.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    [RequireComponent(typeof(PlayerInput))]
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        NormalUpdate();
     }
 
     // Update is called once per frame
-    void Update()
+    void NormalUpdate()
     {
+        // Print statements to be used as placeholders for sounds
+        if (playerInput.actions["gameplay/Jump"].triggered)
+        {
+            print("Jump sound");
+        }
         
+        if (playerInput.actions["gameplay/Strike"].triggered)
+        {
+            print("Strike sound");
+        }
+
+        if (playerInput.actions["gameplay/Pearry"].triggered)
+        {
+            print("Pearry sound");
+        }
+        
+        if (playerInput.actions["gameplay/Interact"].triggered)
+        {
+            if (grabber.GrabItem()) print("Grabbing sound");
+        } 
     }
 }
