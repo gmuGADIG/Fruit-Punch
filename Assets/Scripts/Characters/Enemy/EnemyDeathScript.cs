4a604c3839fc46df8afa5aa377b2615c7e86ff8f
CarJasper <cjasper@gmu.edu>
2024-03-20 21:47:48 -0400

    Basic Enemy Death Fall back
    Made script EnemyDeathScript added it to EnemyCarrot and Enemy Base. Knocks enemy back based on vector then stops and destroys after a time.

--- Assets/Scripts/Characters/Enemy/EnemyDeathScript.cs
+++ Assets/Scripts/Characters/Enemy/EnemyDeathScript.cs
@@ -1,24 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyDeathScript : MonoBehaviour
 {
+    public Vector3 knockBackAngle = new Vector3(1.0f, 0.0f, 0.5f); // future Randomize or base off of last movements
+    public float knockBackForce = 2.0f;
+    [Tooltip("How long enemy is falling")]
+    public float knockBackTime = 0.3f;
+    [Tooltip("How long till Enemy destroy after knock back ends")]
+    public float timeToDeath = 0.8f;
+
     Health myHealth;
+    Enemy enemyAI;
+    Rigidbody rb;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         myHealth = GetComponent<Health>();
+        enemyAI = GetComponent<Enemy>();
+        rb = GetComponent<Rigidbody>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    /// <summary>
+    /// Initiates the Death animations.
+    /// </summary>
+    private void DeathSequence()
     {
+        enemyAI.enabled = false;
+        rb.velocity = Vector3.zero;
+
+        //Add Death anim trigger here
+        rb.AddForce(knockBackAngle * knockBackForce, ForceMode.Impulse);
+
+        Debug.Log("EnemyDied!");
+        Invoke("DeathPart2", knockBackTime);
         
     }
-    void DeathSequence()
+    private void DeathPart2()
     {
-        Debug.Log("EnemyDied!");
+        rb.velocity = Vector3.zero;
+        Destroy(gameObject, timeToDeath);
     }
 
     private void OnEnable()
