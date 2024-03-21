f41976856406ad16efd0ba6e2dd1a3b1eae5fdf8
MMillion-vmat-c <max.fowlkes@gmail.com>
2024-03-20 21:21:06 -0400

    Primary Strike to Skip
    Any player's strike input will skip the waits. I think this is done for the time being?

--- Assets/Scripts/Misc/BasicCutsceneCamera.cs
+++ Assets/Scripts/Misc/BasicCutsceneCamera.cs
@@ -21,6 +21,8 @@ public class BasicCutsceneCamera : MonoBehaviour
 
     public bool cameraMoving = false;
 
+    // panelSkipped is constantly reset to false to prepare for the user(s) to set it to true again.
+    // It gets set to true when any player hits their strike input.
     public bool panelSkipped = false;
 
 
@@ -42,9 +44,8 @@ public class BasicCutsceneCamera : MonoBehaviour
 ///     The game window is 448x252 pixels, so panels ideally should be at least that much, but can go as high 
 ///     resolution as the artist wants (within reason) because the camera can zoom as far out as we want.
 /// 
-///     
-///     TODO: The player should have the option of skipping the hang time by using their primary strike input, immediately going to the next panel
-///     TODO: The player should have a way to skip to the final panel in the array.
+///     Players can use a strike attack to skip wait times and advance panels early if they want.
+///     The cutscene only moves forward, there's no logic for going backwards.
 /// 
 /// 
 /// </summary>
@@ -78,25 +79,23 @@ public class BasicCutsceneCamera : MonoBehaviour
         float elapsedHangTime = 0.0f;
         panelSkipped=false;
 
-
-        while ( elapsedHangTime <= testComic[0].hangTime && panelSkipped==false )
+        while ( elapsedHangTime <= testComic[0].hangTime && !panelSkipped )
         {
             elapsedHangTime+=Time.deltaTime;
+            
             // Wait for the next frame to keep counting.
             yield return null;
         }
 
+        panelSkipped=false;
 
-        //this.transform.position=Vector3.Lerp(transform.position, testComic[1].panelPosition, testComic[1].approachSpeed*Time.deltaTime);
-
-
-        // For each item in the array, i.e., for each panel in your comic, AFTER THE FIRST:
+        // For each item in the array, i.e., for each panel in your comic, AFTER THE FIRST (i = 1):
         for(int i = 1; i < testComic.Length; i++)
         {
             float elapsedTime = 0.0f;
 
             // We lerp on a loop until the lerps are done. i.e., we move the camera.
-            while (elapsedTime < testComic[i].approachTime)
+            while (elapsedTime < testComic[i].approachTime && !panelSkipped)
             {
                 // Lerp POSITION with approachtime to target panel destination
                 transform.position = Vector3.Lerp(transform.position, testComic[i].panelPosition, (elapsedTime / testComic[i].approachTime));
@@ -104,17 +103,21 @@ public class BasicCutsceneCamera : MonoBehaviour
                 comicCamera.orthographicSize = Mathf.Lerp(comicCamera.orthographicSize, testComic[i].cameraScale, (elapsedTime / testComic[i].approachTime));
                 // Keeping track of the change in time for framerate independence.
                 elapsedTime += Time.deltaTime;
-
+                
                 // Wait for the next frame to continue lerping.
                 yield return null;
             }
+            // These next 2 lines are redundant- UNLESS they hit an input to skip the transition.
+            // They're not in a "if panelSkipped" check because they're also a good fallback for safety.
+            transform.position = testComic[i].panelPosition;
+            comicCamera.orthographicSize = testComic[i].cameraScale;
 
             // yield for hangtime before running again
             //OLD: yield return new WaitForSeconds(testComic[i].hangTime);
             //NEW:
             elapsedHangTime = 0.0f;
             panelSkipped=false;
-            while ( elapsedHangTime <= testComic[i].hangTime && panelSkipped==false )
+            while ( elapsedHangTime <= testComic[i].hangTime && !panelSkipped )
             {
                 elapsedHangTime+=Time.deltaTime;
                 // Wait for the next frame to keep counting.
@@ -123,14 +126,8 @@ public class BasicCutsceneCamera : MonoBehaviour
 
             // If there are any panels left, we return to the top of the foor loop on the next panel.
         }
-        // I do remember why I put this in here and it's because IEnumerator wants it apparently.
+        // Wait a frame, though, because IEnumerator said so.
         yield return null;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     void Skip(InputAction.CallbackContext _context)
