559d31594552745771999e10d6325be5b5238a71
Justin1L8 <justin.w.langdon@gmail.com>
2024-03-28 19:08:06 -0400

    prefab + some progress

--- Assets/Scripts/Characters/Boss/Boss.cs
+++ Assets/Scripts/Characters/Boss/Boss.cs
@@ -1,16 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 /// <summary>
 /// Base class for all bosses. <br/>
-/// Does not handle state machine, but provides some functions which subclasses may want to call.
+/// Provides some helpful functions for subclasses, but does NOT handle calling of them. <br/>
+/// The subclass should implement a state machine and determine when to call these functions if needed.
 /// </summary>
 public class Boss : MonoBehaviour
 {
+    [SerializeField] protected float walkSpeed = 20f;
+    protected Rigidbody rb;
+    protected NavMeshAgent navMesh;
+
+    void Start()
+    {
+        this.GetComponentOrError(out rb);
+        this.GetComponentOrError(out navMesh);
+        navMesh.speed = walkSpeed;
+    }
+
     protected IEnumerator WalkToPlayer()
     {
-        // TODO: find player and approach them. exit when close enough or can't get closer.
+        var player =
+            FindObjectsOfType<Player>()
+            .OrderBy(p => Vector3.Distance(this.transform.position, p.transform.position))
+            .First();
+        
+        // TODO: use nav-mesh
         yield return null;
     }
 }
--- Assets/Scripts/Characters/Boss/BossPomela.cs
+++ Assets/Scripts/Characters/Boss/BossPomela.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 enum PomelaState
 {
@@ -8,65 +10,112 @@ enum PomelaState
     Punching,
     Spitting,
     BigJump,
-    Grabbed,
-    
 }
 
 public class BossPomela : Boss
 {
     StateMachine<PomelaState> stateMachine;
+    GroundCheck groundCheck;
     Animator anim;
 
+    [Tooltip("When doing a ground pound jump, Pomela will first jump to one of these points before launching much higher.")]
+    [SerializeField] private Transform[] ropePoints;
+    [SerializeField] private float timeBetweenAttacks = 2;
     [SerializeField] float punchDuration = 2;
     [SerializeField] float spittingDuration = 2;
     [SerializeField] float bigJumpDuration = 2;
+
+    IEnumerator activeCoroutine;
     
     void Start()
     {
+        this.GetComponentOrError(out anim);
+        this.GetComponentOrError(out groundCheck);
+        
         stateMachine = new StateMachine<PomelaState>();
         stateMachine.AddState(PomelaState.Aggressive, null, AggressiveUpdate, null);
-        
-        this.GetComponentOrError(out anim);
+        stateMachine.AddState(PomelaState.Punching, PunchingEnter, null, AttackStateExit);
+        stateMachine.AddState(PomelaState.Spitting, SpittingEnter, null, AttackStateExit);
+        stateMachine.AddState(PomelaState.BigJump, BigJumpEnter, null, AttackStateExit);
     }
 
     PomelaState AggressiveUpdate()
     {
-        PomelaState[] stateOptions = new[] { PomelaState.Punching, PomelaState.Spitting, PomelaState.BigJump};
-        var newState = stateOptions[Random.Range(0, stateOptions.Length)];
-        return newState;
+        if (stateMachine.timeInState >= timeBetweenAttacks)
+        {
+            PomelaState[] stateOptions = { PomelaState.Punching, PomelaState.Spitting, PomelaState.BigJump };
+            var newState = stateOptions[Random.Range(0, stateOptions.Length)];
+            return newState;
+        }
+        else return stateMachine.currentState;
     }
 
     void PunchingEnter()
     {
-        StartCoroutine(Coroutine());
+        activeCoroutine = Coroutine();
+        StartCoroutine(activeCoroutine);
         IEnumerator Coroutine()
         {
             yield return WalkToPlayer();
             anim.Play("Punch"); // TODO: this doesn't exist
             yield return new WaitForSeconds(punchDuration);
-            stateMachine.SetState(PomelaState.Aggressive)
+            stateMachine.SetState(PomelaState.Aggressive);
         }
     }
 
     void SpittingEnter()
     {
-        StartCoroutine(Coroutine());
+        activeCoroutine = Coroutine();
+        StartCoroutine(activeCoroutine);
         IEnumerator Coroutine()
         {
             anim.Play("Spit"); // TODO: make animation exist. add markers in animation for when to shoot.
             yield return new WaitForSeconds(spittingDuration);
-            stateMachine.SetState(PomelaState.Aggressive)
+            stateMachine.SetState(PomelaState.Aggressive);
         }
     }
 
     void BigJumpEnter()
     {
-        StartCoroutine(Coroutine());
+        activeCoroutine = Coroutine();
+        StartCoroutine(activeCoroutine);
         IEnumerator Coroutine()
         {
             anim.Play("Jump"); // TODO: jump to serializable jump positions, then jump near player
             yield return new WaitForSeconds(bigJumpDuration);
             stateMachine.SetState(PomelaState.Aggressive);
+        }
+    }
+
+    /// <summary>  Exit callback for all attack states. Ends the active coroutine. </summary>
+    void AttackStateExit(PomelaState prevState) => StopCoroutine(activeCoroutine);
+
+    IEnumerator JumpTo(Vector3 targetPosition, float height, float gravity)
+    {
+        float jumpTime = 2 * Mathf.Sqrt(2 * height / gravity);
+        float startVelocity = Mathf.Sqrt(2 * height / gravity) * gravity;
+
+        Vector3 startPosition = rb.position;
+        rb.velocity += Vector3.up * startVelocity;
+        
+        float elapsedTime = 0;
+        while (elapsedTime < jumpTime / 2 || !groundCheck.IsGrounded())
+        {
+            float t = elapsedTime / jumpTime;
+            rb.position = new Vector3(
+                Mathf.Lerp(startPosition.x, targetPosition.x, t),
+                rb.position.y,
+                Mathf.Lerp(startPosition.z, targetPosition.z, t)
+            );
+            rb.velocity += Vector3.down * gravity * Time.deltaTime;
+            
+            yield return new WaitForEndOfFrame();
+
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime > 10f) { // safety exit
+                Debug.LogWarning("Safety Exit: big jump didn't land after 10 seconds");
+                break;
+            }
         }
     }
 }
