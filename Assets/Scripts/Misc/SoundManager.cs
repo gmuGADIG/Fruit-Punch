2e0a30b8c2636d419d513b174bcf65389df6f817
Connor Harrison <ctharrison04@gmail.com>
2024-03-27 21:05:15 -0400

    Sound now plays on match with inputted string

--- Assets/Scripts/Misc/SoundManager.cs
+++ Assets/Scripts/Misc/SoundManager.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+[RequireComponent(typeof(AudioSource))]
 public class SoundManager : MonoBehaviour
 {
     // placeholder
-    public string sound = "playerJump";
+    public string sound = "ArcadeTest";
 
     // May be redeveloped in a cleaner Soundbank.cs file
-    private Sound[] soundBank;
+    public AudioClip[] soundBank;
 
     public static SoundManager Current { get; private set; }
 
@@ -25,11 +25,11 @@ public class SoundManager : MonoBehaviour
         }
         // warnings should go away when a sound name is linked?
         // May be redeveloped in a cleaner Soundbank.cs file
-        soundBank[0] = new Sound(0, "playerWalk", AudioClip.Create("ArcadeTest.wav", 44100 * 2, 1, 44100, true, true, OnAudioRead));
-        soundBank[1] = new Sound(1, "playerJump", AudioClip.Create("ArcadeTest.wav", 44100 * 2, 1, 44100, true, true, OnAudioRead));
-        soundBank[2] = new Sound(2, "playerStrike", AudioClip.Create("ArcadeTest.wav", 44100 * 2, 1, 44100, true, true, OnAudioRead));
-        soundBank[3] = new Sound(3, "playerPearry", AudioClip.Create("ArcadeTest.wav", 44100 * 2, 1, 44100, true, true, OnAudioRead));
-        soundBank[4] = new Sound(4, "playerDie", AudioClip.Create("ArcadeTest.wav", 44100 * 2, 1, 44100, true, true, OnAudioRead));
+        //soundBank[0] = new Sound(0, "playerWalk", AudioClip.Create("ArcadeTest.wav", 44100 * 2, 1, 44100, true, true, OnAudioRead));
+        //soundBank[1] = new Sound(1, "playerJump", AudioClip.Create("ArcadeTest.wav", 44100 * 2, 1, 44100, true, true, OnAudioRead));
+        //soundBank[2] = new Sound(2, "playerStrike", AudioClip.Create("ArcadeTest.wav", 44100 * 2, 1, 44100, true, true, OnAudioRead));
+        //soundBank[3] = new Sound(3, "playerPearry", AudioClip.Create("ArcadeTest.wav", 44100 * 2, 1, 44100, true, true, OnAudioRead));
+        //soundBank[4] = new Sound(4, "playerDie", AudioClip.Create("ArcadeTest.wav", 44100 * 2, 1, 44100, true, true, OnAudioRead));
     }
     public void Update()
     {
@@ -38,11 +38,14 @@ public class SoundManager : MonoBehaviour
     }
     public static void playSound(string sound)
     {
-        foreach(Sound s in Current.soundBank)
+        foreach(AudioClip s in Current.soundBank)
         {
-            if (s.getSoundInfo().Contains(sound))
+            if (s.name.Contains(sound))
             {
-                Debug.Log("Found match with " + s.getSoundInfo());
+                Debug.Log("Found match with " + s.name);
+                AudioSource audio = Current.GetComponent<AudioSource>();
+                audio.clip = s;
+                audio.Play();
                 return;
             }
         }
