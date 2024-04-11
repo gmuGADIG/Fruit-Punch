f79c4f0c976aff14e0fb88d2d043f52501b7de1f
Tushar <stuffcopresident@gmail.com>
2024-04-10 21:59:31 -0400

    Started work again on spawners with vel field.

--- Assets/Scripts/Environment/Spawner/EnemySpawns.cs
+++ Assets/Scripts/Environment/Spawner/EnemySpawns.cs
@@ -37,8 +37,24 @@ public class EnemySpawns : MonoBehaviour
         return copy;
     }
 
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
+        // Get a line of points from the spawner, in the direction of the velocity over time minus gravity to show the path the enemy will take.
+        Vector3[] points = new Vector3[50];
+        points[0] = transform.position;
+        Vector3 velocity = spawnVelocity;
+        for (int i = 1; i < points.Length; i++)
+        {
+            velocity += Physics.gravity * Time.fixedDeltaTime;
+            points[i] = points[i - 1] + velocity * Time.fixedDeltaTime;
+        }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(points[points.Length - 1], 0.5f);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLineList(points);
 
     }
     
