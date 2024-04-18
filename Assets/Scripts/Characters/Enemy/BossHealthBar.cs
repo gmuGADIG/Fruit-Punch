00c2f77cfcc6c441075df27e2419826fc5c649df
Patrick <logicalrocketship@gmail.com>
2024-04-17 20:53:19 -0400

    BossHealthBar Script Added
    The BossHealthBar script extends teh EnemyHealthBar script, allowing for similar functions to normal Enemy's Health component.
    Update() needs to be implemented to keep health bar at top of screen.

---
+++ Assets/Scripts/Characters/Enemy/BossHealthBar.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealthBar : EnemyHealthBar
+{
+    // Override -- Health Bar displays at top of screen instead of bottom
+    void Update()
+    {
+        // Add script to put health bar on top of screen
+    }
+}
