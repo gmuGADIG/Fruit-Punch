7fa842c94da9eb6aadb5728a505b6e26428286ac
KaratePikachu <KaidenKeyZ@gmail.com>
2024-04-17 20:03:08 -0400

    Pre-category adding

--- Assets/Scripts/UI/CodexManager.cs
+++ Assets/Scripts/UI/CodexManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -15,43 +16,48 @@ public class Codex : MonoBehaviour
     [SerializeField]
     public Transform buttonsParent;
 
+    public enum Category
+    {
+        ChangeMeOne,
+        ChangeMeTwo,
+        ChangeMeThree,
+        ChangeMeFour
+    }
+
     [System.Serializable]
     public class Entry
     {
+        public Category category;
         public string name;
         public string description;
         public Sprite image;
-        [HideInInspector]
-        public GameObject button;
+        //[HideInInspector]
+        //public GameObject button; Not nessesary at the current moment
         [HideInInspector]
         public RectTransform buttonTransform;
-    }
-
-    private enum Mode
-    {
-        Waiting,
-        Changing
     }
 
     public List<Entry> entries = new List<Entry>();
     private int index;
     private int nextIndex;
-    private Mode mode = Mode.Waiting;
+    private bool switching = false;
     private Vector3 smallButton = new Vector3(160, 30);
     private Vector3 largeButton = new Vector3(160, 50);
 
     private void Awake()
     {
+        //int numCategories = Enum.GetNames(typeof(Category)).Length;
+
         index = 0;
         for (int i=0; i<entries.Count; i++)
         {
             int iCopy = i;//This is incredibly jank, but when adding listeners in a loop, all of the buttons will be last number of i without a temp var.
 
             Entry e = entries[i];
-            e.button = GameObject.Instantiate(buttonPrefab,Vector3.zero,Quaternion.identity,buttonsParent);
-            e.button.GetComponent<Button>().onClick.AddListener(delegate { ButtonClicked(iCopy); });
-            e.button.GetComponentInChildren<TextMeshProUGUI>().text = e.name;
-            e.buttonTransform = e.button.GetComponent<RectTransform>();
+            GameObject button = GameObject.Instantiate(buttonPrefab,Vector3.zero,Quaternion.identity,buttonsParent);
+            button.GetComponent<Button>().onClick.AddListener(delegate { ButtonClicked(iCopy); });
+            button.GetComponentInChildren<TextMeshProUGUI>().text = e.name;
+            e.buttonTransform = button.GetComponent<RectTransform>();
         }
 
         entries[0].buttonTransform.sizeDelta = largeButton;
@@ -72,7 +78,7 @@ public class Codex : MonoBehaviour
         ResizeButton(nextIndex,smallButton, largeButton, 1.0f);
 
         index = nextIndex;
-        mode = Mode.Waiting;
+        switching = false;
         StopCoroutine("ChangeSelected");
     }
 
@@ -88,8 +94,8 @@ public class Codex : MonoBehaviour
 
     private void ButtonClicked(int bindex)
     {
-        if (bindex!=index&&mode==Mode.Waiting) {
-            mode = Mode.Changing;
+        if (bindex!=index&&!switching) {
+            switching = true;
             StartCoroutine("ChangeSelected", bindex);
         }
     }
