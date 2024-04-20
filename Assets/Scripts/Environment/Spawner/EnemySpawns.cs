38d9219944f751ea9add0511ecdad02b62d15a52
Tushar <stuffcopresident@gmail.com>
2024-04-20 18:16:45 -0400

    Updated gizmos to better view spawnpoint data

--- Assets/Scripts/Environment/Spawner/EnemySpawns.cs
+++ Assets/Scripts/Environment/Spawner/EnemySpawns.cs
@@ -40,22 +40,39 @@ public class EnemySpawns : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
+        const float gizmoSize = 0.1f;
+
         // Get a line of points from the spawner, in the direction of the velocity over time minus gravity to show the path the enemy will take.
         Vector3[] points = new Vector3[50];
         points[0] = transform.position;
         Vector3 velocity = spawnVelocity;
         for (int i = 1; i < points.Length; i++)
         {
-            velocity += Physics.gravity * Time.fixedDeltaTime;
-            points[i] = points[i - 1] + velocity * Time.fixedDeltaTime;
+            velocity += Physics.gravity * Time.fixedDeltaTime*2;
+            points[i] = points[i - 1] + velocity * Time.fixedDeltaTime*2;
+            if (Physics.Raycast(points[i-1], velocity.normalized, out RaycastHit hit, velocity.magnitude * Time.fixedDeltaTime*2))
+            {
+                points[i] = hit.point;
+                break;
+            }
         }
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, 0.5f);
+        //if the array is full, the enemy never hit anything, so draw a line to the last point and log a warning
+        if (points[points.Length - 1] != Vector3.zero)
+        {
+            Debug.LogWarning("Enemy path was not interrupted by a collider. Enemy will continue off screen and never spawn.");
+            Gizmos.color = Color.red;
+        }
+        else
+        {
+            //rezise the array to the number of points that were actually used
+            System.Array.Resize(ref points, System.Array.IndexOf(points, Vector3.zero));
+            Gizmos.color = Color.yellow;
+        }
+        Gizmos.DrawSphere(transform.position, gizmoSize);
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(points[points.Length - 1], 0.5f);
+        Gizmos.DrawSphere(points[points.Length - 1], gizmoSize);
         Gizmos.color = Color.blue;
-        Gizmos.DrawLineList(points);
+        Gizmos.DrawLineStrip(points, false);
 
     }
     
