8b492e9127e76abb6ebf0fae68509f1967417783
Patrick <logicalrocketship@gmail.com>
2024-04-24 20:29:58 -0400

    Enemy Health Permission changed to private
    Looks like the change to protected wasn't necessary...
    
    The bug fix was tested numerous times, and it works!

--- Assets/Scripts/Characters/Enemy/EnemyBehaviors/Enemy.cs
+++ Assets/Scripts/Characters/Enemy/EnemyBehaviors/Enemy.cs
@@ -79,7 +79,7 @@ public class Enemy : MonoBehaviour
     protected GroundCheck groundCheck;
     protected Grabbable grabbable;
     protected Rigidbody rb;
-    protected Health health;
+    private Health health;
     protected StateMachine<EnemyState> stateMachine = new();
 
     [SerializeField] protected GameObject debugMarkerPrefab;
